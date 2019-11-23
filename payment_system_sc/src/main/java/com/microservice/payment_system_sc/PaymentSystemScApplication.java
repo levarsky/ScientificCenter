c8275fa6ee231199993f0cffb9662be0ff0d7274
Levarsky <Mac>
2019-11-23 16:06:14 +0100

    SC Payment Service

---
+++ payment_system_sc/src/main/java/com/microservice/payment_system_sc/PaymentSystemScApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.payment_system_sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemScApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSystemScApplication.class, args);
+    }
+
+}
