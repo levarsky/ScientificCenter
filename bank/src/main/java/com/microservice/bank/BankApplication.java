abe505263d1c5ae64cc3de3ff907d4d5ae04479c
Levarsky <Mac>
2019-11-23 14:58:01 +0100

    SC Payment Service

--- bank/src/main/java/com/microservice/bank/BankApplication.java
+++ bank/src/main/java/com/microservice/bank/BankApplication.java
@@ -2,10 +2,9 @@ package com.microservice.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
 public class BankApplication {
 
     public static void main(String[] args) {
