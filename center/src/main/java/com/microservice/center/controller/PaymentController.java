df4c49c27ee5f015c233eb4ad17401f149e63e05
Levarsky <Mac>
2019-12-21 19:39:45 +0100

    Center

--- center/src/main/java/com/microservice/center/controller/PaymentController.java
+++ center/src/main/java/com/microservice/center/controller/PaymentController.java
@@ -3,13 +3,11 @@ package com.microservice.center.controller;
 import com.microservice.center.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pay")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PaymentController {
 
     @Autowired
