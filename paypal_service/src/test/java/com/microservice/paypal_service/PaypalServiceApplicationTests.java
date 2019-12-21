a37ecb0ac19b5b81a4b8e60c8d10b8e646bd4576
Levarsky <Mac>
2019-12-21 17:15:32 +0100

    SERVICES

---
+++ paypal_service/src/test/java/com/microservice/paypal_service/PaypalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.paypal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sellers-service/src/main/java/com/microservice/sellers_service/SellersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.sellers_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellersServiceApplication.class, args);
+    }
+
+}
