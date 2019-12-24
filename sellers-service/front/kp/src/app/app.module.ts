c59d7da784cc2dc11b8622b5efcdc8368d25b43d
Vekisa <vmosorinski@gmail.com>
2019-12-24 22:03:49 +0100

    zbog prebacaja kompa

--- sellers-service/front/kp/src/app/app.module.ts
+++ sellers-service/front/kp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BankComponent } from './bank/bank.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BankComponent
   ],
   imports: [
     BrowserModule,
--- sellers-service/src/main/java/com/microservice/sellers_service/controller/ClientController.java
+++ sellers-service/src/main/java/com/microservice/sellers_service/controller/ClientController.java
@@ -1,9 +1,19 @@
 package com.microservice.sellers_service.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
 
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createCommunication() {
+       //Treba da primi predefinisani objekat koji sadrzi kolicinu novca, response je redirekcija na front za
+        //izbor nacina placanja
+        return null;
+    }
 
 }
